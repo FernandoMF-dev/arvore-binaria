e6fe6d94c798337de83f4e95715fc6ebf4d7d8ae
Fernando Martins Ferreira <fernando.ferreira@basis.com.br>
2021-11-17 10:39:27 -0300

    Preparando terreno para testar ArvoreAvl
    Signed-off-by: Fernando Martins Ferreira <fernando.ferreira@basis.com.br>

--- arvore-binaria-avl/main.c
+++ arvore-binaria-avl/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 #include "headers/Aluno.h"
-#include "headers/ArvoreBinaria.h"
+#include "headers/ArvoreAvl.h"
 #include "headers/ControleArquivoAluno.h"
 
 int menu() {
@@ -9,9 +9,20 @@ int menu() {
 
     printf("\n\n=============== MENU ===============\n");
     printf("\n1 - Inserir");
-    printf("\n2 - Busca");
-    printf("\n3 - Remover");
-    printf("\n4 - Estatística");
+    printf("\n2 - Recuperar");
+    printf("\n3 - Busca e Imprimir");
+    printf("\n4 - Remover");
+    printf("\n5 - Altura");
+    printf("\n6 - Tamanho");
+    printf("\n7 - Maior");
+    printf("\n8 - Menor");
+    printf("\n9 - Limpar");
+    printf("\n10 - Estatística");
+    printf("\n11 - Listar Em Ordem ASC");
+    printf("\n12 - Listar Em Ordem DES");
+    printf("\n13 - Listar Pré-Ordem");
+    printf("\n14 - Listar Pós-Ordem");
+    printf("\n15 - Clone");
     printf("\n0 - Sair");
 
     printf("\nESCOLHA: ");
@@ -21,39 +32,82 @@ int menu() {
 }
 
 int main() {
-    ArvoreBinaria *arvoreBinaria = readAlunoFromFile(newArvoreBinaria("ArvoreBinaria"));
+    ArvoreAvl *arvoreAvl = newArvoreAvl("ArvoreAvl");
+    ArvoreAvl *clone = newArvoreAvl("Clone");
     char *key = (char *) malloc(64 * sizeof(char));
     int repeat = 1;
 
     while (repeat) {
         switch (menu()) {
             case 0:
-                writeArvoreBinariaOnFile(arvoreBinaria);
                 repeat = 0;
                 break;
             case 1:
-                insertArvoreBinaria(arvoreBinaria, readAluno());
+                insertArvoreAvl(arvoreAvl, readAluno());
                 break;
             case 2:
                 printf("\nENTRADA: ");
                 scanf(" %[^\n]s", key);
                 printf("\nSAÍDA: ");
-                findAndPrintArvoreBinaria(arvoreBinaria, key);
+                printAluno(searchArvoreAvl(arvoreAvl, key));
                 break;
             case 3:
                 printf("\nENTRADA: ");
                 scanf(" %[^\n]s", key);
-                removeArvoreBinaria(arvoreBinaria, key);
+                printf("\nSAÍDA: ");
+                findAndPrintArvoreAvl(arvoreAvl, key);
                 break;
             case 4:
-                printStatisticsArvoreBinaria(arvoreBinaria);
+                printf("\nENTRADA: ");
+                scanf(" %[^\n]s", key);
+                removeArvoreAvl(arvoreAvl, key);
+                break;
+            case 5:
+                printf("SAÍDA: %d", getHeightArvoreAvl(arvoreAvl));
+                break;
+            case 6:
+                printf("SAÍDA: %d", getSizeArvoreAvl(arvoreAvl));
+                break;
+            case 7:
+                printf("\nSAÍDA: ");
+                printAluno(getMaxArvoreAvl(arvoreAvl));
+                break;
+            case 8:
+                printf("\nSAÍDA: ");
+                printAluno(getMinArvoreAvl(arvoreAvl));
+                break;
+            case 9:
+                clearArvoreAvl(arvoreAvl);
+                break;
+            case 10:
+                printStatisticsArvoreAvl(arvoreAvl);
+                printStatisticsArvoreAvl(clone);
+                break;
+            case 11:
+                printArvoreAvlInOrderAsc(arvoreAvl);
+                printArvoreAvlInOrderAsc(clone);
+                break;
+            case 12:
+                printArvoreAvlInOrderDesc(arvoreAvl);
+                printArvoreAvlInOrderDesc(clone);
+                break;
+            case 13:
+                printArvoreAvlPreOrder(arvoreAvl);
+                printArvoreAvlPreOrder(clone);
+                break;
+            case 14:
+                printArvoreAvlPostOrder(arvoreAvl);
+                printArvoreAvlPostOrder(clone);
+                break;
+            case 15:
+                cloneArvoreAvl(clone, arvoreAvl);
                 break;
             default:
                 break;
         }
     }
 
-    clearArvoreBinaria(arvoreBinaria);
-    free(arvoreBinaria);
+    clearArvoreAvl(arvoreAvl);
+    free(arvoreAvl);
     return 0;
 }
